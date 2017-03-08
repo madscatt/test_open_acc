dc6de1d5110d54f2c0c61bc5793e3b3f0e3aa84f
Joseph E. Curtis <madscatt@gmail.com>
2017-03-08 13:25:23 -0500

    state save

--- pr/extensions/openacc/first_try_local_hist_oacc_pr.cpp
+++ pr/extensions/openacc/first_try_local_hist_oacc_pr.cpp
@@ -19,13 +19,11 @@ void get_distances(double ***coor, const int nframes, const int natoms, std::vec
 //void get_distances(double ***coor, const int nframes, const int natoms, std::vector<double>& dist) {
 //void get_distances(double ***coor, const int nframes, const int natoms, double* dist) {
 
-    int i,j,k,l ;
-    unsigned long long npairs, nfnp ;
+    int i,j,k ;
+    unsigned long long npairs ;
     double x1, y1, z1, x2, y2, z2, sdist ;
     double dx2, dy2, dz2 ;
-    double this_low_bin, this_high_bin ;
     unsigned long long local_count ; 
-    unsigned long long local_hist[nbins] ; 
     unsigned long long z ; 
 
     /// temporary printing for debugging
@@ -35,35 +33,32 @@ void get_distances(double ***coor, const int nframes, const int natoms, std::vec
     std::string remark = "#hello";
     const std::string filename = "dum_oacc.txt";
     
-    //std::ofstream output_file(filename.c_str()) ;
-    std::ofstream output_file ;
     std::ofstream outfile(filename.c_str()) ;
     outfile << remark << std::endl;
 
     //
-  
+   
     printf("oacc: %d\n", nframes) ;
     printf("oacc: %d\n", natoms) ;
     printf("oacc: coor[0][0][0] = %f\n", coor[0][0][0]) ;
     npairs = (natoms * (natoms - 1))/2 ;
     double dist[npairs] ;
-    double local_dist[npairs] ; 
-    
-    std::cout << "nframes * npairs = " << nframes*npairs << std::endl ;
-    nfnp = nframes*npairs ;
+    double local_dist[nframes*npairs] ;
+
+    std::cout << "zero-ing local_dist array" << std::endl ; 
     std::cout << "nf * np = " << nframes * npairs << std::endl ;
     std::cout << "nf = " << nframes << std::endl ;
     std::cout << "np = " << npairs << std::endl ;
 
-    output_file.open("dum.txt") ;
+    for(z=0; z < nframes*npairs ; z++){
+        //std::cout << z << std::flush ; 
+        //local_dist[z] = 0.0 ;
+    }
 
-    std::cout << "starting parallel loops" << std::endl ; 
-    #pragma acc data copyin(coor[nframes][natoms][3]) copy(dist[npairs], local_dist[npairs])
+    std::cout << "starting parallel loops" << std::flush ; //std::endl ; 
+    #pragma acc data copyin(coor[nframes][natoms][3]) copy(dist[npairs], local_dist[nframes*npairs])
     {
     for(i=0 ; i < nframes ; i++){
-        for(z=0; z < npairs ; z++) { local_dist[z] = 0.0 ; }
-        for(j=0; j < nbins ; j++) { local_hist[j] = 0 ; }
-        std::cout << "." << std::flush ;
         #pragma acc parallel loop
         {
         for(j=0 ; j < natoms-1 ; j++){
@@ -82,51 +77,15 @@ void get_distances(double ***coor, const int nframes, const int natoms, std::vec
                 sdist = sqrt(dx2 + dy2 + dz2) ;
                 local_count = ((j*natoms)-((j*(j+1))/2))+k-(j+1) ;
                 dist[local_count] += sdist ; 
-                local_dist[local_count] = sdist ; 
-
-                for(l=0; l<nbins; l++){
-                    this_low_bin = double(l)*bin_width ;
-                    this_high_bin = this_low_bin + bin_width ;
-                    if(sdist > this_low_bin && sdist <= this_high_bin){
-                        local_hist[l] += 1 ;
-                        break;
-                    }
-                } // end of l-loop
-
+                local_dist[(i * npairs) + local_count] += sdist ; 
             } // end of k-loop
             } // end of pragma acc loop
         } // end of j-loop
         } // end of pragma acc parallel loop
-        
-        //for(j=0 ; j < npairs ; j++){
-         //   for(k=0 ; k < nbins ; k++){
-          //      this_low_bin = double(k)*bin_width ;
-           //     this_high_bin = this_low_bin + bin_width ;
-            //    if(local_dist[j] > this_low_bin && local_dist[j] <= this_high_bin){
-             //       hist[k] += 1 ;
-              //      local_hist[k] += 1 ;
-               //     break ;
-                //}
-           // }
-        //}
-   
-        if(i<nframes){ 
-            //outfile << sstream ;
-            for(k=0 ; k < nbins ; k++){
-                output_file << local_hist[k] << "\n" ;
-            //sstream << dist[z]/double(nframes) << endl;
-            }
-            output_file << "#\n" ;
-            output_file.flush() ;
-        }
-
     } // end of i-loop
     } // end of pragma acc data 
 
-    std::cout << std::endl ;
-
-    output_file.close();
-    
+    double this_low_bin, this_high_bin ;
     //std::vector<int> hist(nbins,0);
 
     std::cout << "creating histogram: 1" << std::endl ;
@@ -196,9 +155,6 @@ void get_distances(double ***coor, const int nframes, const int natoms, std::vec
     }
     //outfile << sstream ;
  */     
-
-    //delete[] local_dist ;
-    free(local_dist) ;
 
     return ;
 }
