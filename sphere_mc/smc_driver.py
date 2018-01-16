3044e83cd6cdf2daafca8cb8d779e01026377368
Joseph E. Curtis <madscatt@gmail.com>
2018-01-16 13:31:34 -0500

    replaced pr_driver with smc_driver code derived from sphere_mc.py

--- sphere_mc/smc_driver.py
+++ sphere_mc/smc_driver.py
@@ -1,107 +1,188 @@
 import sasmol.sasmol as sasmol
-import numpy
-import pr_parallel as pr_parallel
-import math
-import sys
+import numpy,math,random
+import os,sys
 
-try:
-    import prc as prc
-    flag = True
-except:
-    print 'could not import prc'
-    flag = False
+#import smc_parallel
 
-def get_number_of_bins_from_structure(m, input_nbins):
+sys.path.append("./")
 
-    minmax = m.calc_minmax_all_steps(dcdfile)
- 
-    dx = minmax[1][0] - minmax[0][0]
-    dy = minmax[1][1] - minmax[0][1]
-    dz = minmax[1][2] - minmax[0][2]
+import make_ring as make_ring
 
-    rmax = math.sqrt(dx*dx + dy*dy + dz*dz)
+class parameters():
 
-    structure_nbins = int(math.ceil(rmax / bin_width))
+        def __init__(self):
+		self.number_of_steps = 100
+		self.temperature = 3.0
+		self.rho = 0.65
+		self.natoms = 701
+		self.sigma_1 = 1.0
+		self.sigma_2 = 1.0
+		self.radius = 30.0
 
-    if structure_nbins > input_nbins:
-        nbins = structure_nbins
-    else:
-        nbins = input_nbins
+		self.epsilon_ab_a = 1.0
+		self.epsilon_ab_r = 1.0
+		self.Rab_a = 1.0	
+		self.Rab_r = 2.0	
+		self.sigma_ab = 1.0
+		self.energy = 1E99
+                self.beta = 1.0 # Boltzman temperature
 
-    return nbins
+def calc_dist(coor1,coor2):
 
-def calculate_pr(pdbfile, dcdfile, input_nbins, bin_width):
+	dx = coor1[0] - coor2[0]
+	dy = coor1[1] - coor2[1]
+	dz = coor1[2] - coor2[2]
 
-    m = sasmol.SasMol(0)
-    m.read_pdb(pdbfile)
+	r = math.sqrt(dx*dx + dy*dy + dz*dz)
 
-    nbins = get_number_of_bins_from_structure(m, input_nbins)
-    
-    print 'nbins = ', nbins
-    
-    m.read_dcd(dcdfile)
-    nf = m.number_of_frames()
-    print 'nf = ', nf
+	return r
 
-    coor = m.coor()
-    print 'coor[0][0] = ', coor[0][0]
-    print 'coor[0][-1] = ', coor[0][-1]
+def energy(mol,p):
 
-    natoms = m.natoms()
-    print 'natoms = ', natoms
+        ''' 
+        method to calculate energy.  If particles overlap then it returns
+        a boolean False, otherwise it returns the sum of the long-range energy
+        '''
 
-    if flag:
-        npairs = (natoms * (natoms - 1))/2
-        all_distances = numpy.zeros(npairs, numpy.float32)
-        for i in xrange(nf): 
-            distances = prc.prc(coor[i])
-            all_distances += numpy.array(distances)
-            if i==0:
-                print 'one_distance[0] = ',distances[0]
-            print '.',
-     
-        print 'all_distances[0] = ', all_distances[0]/nf
+	u_long_range = 0.0 
 
-    print 'calling pr_parallel'
+	for i in xrange(p.natoms-1):
+#		print i+1,	
+#		sys.stdout.flush()
 
-    print 'python: coor[0][0][0] = ', coor[0][0][0]
-    dist = pr_parallel.pr_parallel(coor,nf,natoms,nbins,bin_width)
-    
-    print '\nback in python\n\n'
-    
-    outfile = open('final_python_dist.txt','w')
-    #for val in dist:
-    for i in xrange(nf):
-        for j in xrange(nbins):
-            outfile.write('%i\t%d\n' % (j, dist[i][j])) 
+		coor_i = mol.coor()[0][i][:]	
+			
+		for j in xrange(i+1,p.natoms):
 
-    outfile.close()
+			coor_j = mol.coor()[0][j][:]
+
+			r = calc_dist(coor_i,coor_j)
+
+                        if r < p.sigma_1 + p.sigma_2:
+	                    return False
+                
+			u_long_range_1 = -p.epsilon_ab_a * ( ( p.sigma_ab / p.Rab_a )**2.0 ) * math.exp(-(r/p.Rab_a))
+			u_long_range_2 =  p.epsilon_ab_r * ( ( p.sigma_ab / p.Rab_r )**2.0 ) * math.exp(-(r/p.Rab_r))
+
+			u_long_range += u_long_range_1 + u_long_range_2
+
+
+	return u_long_range
+
+def surface_move(mol,p,i):
+
+#		x = R sin(th) cos(phi)
+#		y = R sin(th) sin(phi)
+#		z = R cos(th)
+
+	max_dtheta = 0.01
+	max_dphi = 0.01
+
+	x = mol.coor()[0][i][0]
+	y = mol.coor()[0][i][1]
+	z = mol.coor()[0][i][2]
+
+	r = math.sqrt(x*x+y*y+z*z)
+
+#	theta = math.acos(z/r)
+#	phi = math.atan(y/x)
+	
+#	ran_theta = max_dtheta*random.uniform(-1.0,1.0)
+#	ran_phi = max_dphi*random.uniform(-1.0,1.0)
+
+#	dx = r * math.sin(theta+ran_theta)*math.cos(phi+ran_phi)
+#	dy = r * math.sin(theta+ran_theta)*math.sin(phi+ran_phi)
+#	dz = r * math.cos(theta+ran_theta)
+
+	max_disp = 0.05
+	dx = max_disp * random.uniform(-1.0,1.0)
+	dy = max_disp * random.uniform(-1.0,1.0)
+	dz = max_disp * random.uniform(-1.0,1.0)
+
+	x += dx ; y += dy ; z += dz
+	norm = math.sqrt(x*x + y*y + z*z)
+	x *= r/norm
+	y *= r/norm
+	z *= r/norm
+
+        accepted = False
+
+        u_long_range = energy(mol, p)
+        
+        if u_long_range:
+            if u_long_range < p.energy:
+                accepted = True
+            else:
+                delta_energy = u_long_range - p.energy
+                boltz = math.exp(-p.beta * delta_energy)
+                ran = random.random()
+                if ran < boltz:
+                    accepted = True
+
+        if accepted:
+            p.energy = u_long_range
+	    mol.coor()[0][i][0] = x
+	    mol.coor()[0][i][1] = y
+	    mol.coor()[0][i][2] = z
+	
+        return
+
+def mc_run(restartpdb):
+
+	p = parameters()
+        
+        if restartpdb:
+	    print '>> reading initial configuration from pdb file'
+            if restartpdb:
+                mol = sasmol.SasMol(0)
+                mol.read_pdb(restartpdb)
+        else:
+            print '>> setting up initial configuration'
+        
+	    mol = make_ring.ring(p.radius*2,p.natoms,p.sigma_1*2.0)
+
+	p.natoms = mol.natoms()
+	mol.center(0)
+	print '>> natoms = ',p.natoms
+
+	dcdoutfile = mol.open_dcd_write("traj.dcd")
+
+#	u_long_range = energy(mol,p)
+
+#	print 'energy = ',u_long_range
+
+	for step in xrange(p.number_of_steps):
+		print step,
+		sys.stdout.flush()
+		
+		for i in xrange(p.natoms):
+				
+			surface_move(mol,p,i)
+		mol.center(0)
+		mol.write_dcd_step(dcdoutfile,0,step)
+
+	mol.close_dcd_write(dcdoutfile)
+	mol.write_pdb("final_coor.pdb",0,"w")
+
+	return
 
 if __name__ == "__main__":
 
-    nbins = 200
-    bin_width = 1.0
+	restartpdb = False
+	restartpdb = 'initial_structure.pdb'
+    import time¬
+    start_time = time.time()¬
+    mc_run(restartpdb)
+    elapsed_time = time.time() - start_time¬
+    print 'elapsed time = ', elapsed_time¬
+   
+    # hmm, replae this in the future; means nothing for now
 
-    pdbfile = 'ten_mer.pdb'
-    #pdbfile = 'n.pdb'
-    #pdbfile = 'nist_mab.pdb'
-    #pdbfile = 'bent_fg_from_constrained_md.pdb'
-    pdbfile = 'backbone_bent_fg.pdb'
- 
-    dcdfile = 'ten_mer.dcd'
-    dcdfile = 'ten_mer_4591.dcd'
-    #dcdfile = 'n200.dcd'
-    #dcdfile = 'n1000.dcd'
-    #dcdfile = 'n10000.dcd'
-    #dcdfile = 'xray_x2_lt_55.dcd'
-    #dcdfile = 'cdyn0.dcd'
-    dcdfile = 'backbone_bent_fg_cdyn0.dcd'
+    nframes = 1000¬
+    print 'seconds per frame (1000) = ', elapsed_time/nframes¬
 
-    import time
-    start_time = time.time()
-    calculate_pr(pdbfile, dcdfile, nbins, bin_width)
-    elapsed_time = time.time() - start_time
-    print 'elapsed time = ', elapsed_time
-    nframes = 1000
-    print 'seconds per frame (1000) = ', elapsed_time/nframes
 
+
+
+
+	print '\n\n>>> DONE <<<\n\n'
